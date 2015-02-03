351a6ce6396535ad7b3a2658e6d09a43b74a314e
Robin Joseph <robinjoseph08@sbcglobal.net>
2015-02-03 11:37:57 -0600

    clean up commented out code

--- app/assets/javascripts/views/photos.js
+++ app/assets/javascripts/views/photos.js
@@ -1,41 +1,12 @@
 App.PhotosView = Ember.View.extend({
 
   didInsertElement: function () {
-    // $('.owl-carousel').owlCarousel({
-    //   singleItem: true,
-    //   autoHeight: true
-    // });
-    // var settings = {
-    //   viewport: [
-    //     {
-    //       width: 1200,
-    //       columns: 5
-    //     }, {
-    //       width: 900,
-    //       columns: 4
-    //     }, {
-    //       width: 500,
-    //       columns: 3
-    //     }, {
-    //       width: 320,
-    //       columns: 2
-    //     }, {
-    //       width: 0,
-    //       columns: 2
-    //     }
-    //   ],
-    //   historyapi: false
-    // };
-
-    // Gamma.init(settings);
     var selector = '.gallery';
     var $container = $(selector);
-    // initialize
     var msnry = new Masonry(selector, {
       columnWidth: 200,
       itemSelector: '.item',
-      isFitWidth: true,
-      // isInitLayout: false
+      isFitWidth: true
     });
     $container.imagesLoaded( function() {
       msnry.layout()
