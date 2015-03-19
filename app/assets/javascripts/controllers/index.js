1014b37af3f28bf069b10403c90585bc45e8c929
Robin Joseph <robinjoseph08@sbcglobal.net>
2015-03-19 13:50:25 -0500

    add countdown to index

--- app/assets/javascripts/controllers/index.js
+++ app/assets/javascripts/controllers/index.js
@@ -1,5 +1,19 @@
 App.IndexController = Ember.Controller.extend({
 
-  needs: ['application']
+  needs: ['application'],
+
+  currentTimeMetronome: function() {
+    var interval = 1000;
+    Ember.run.later(this, function() {
+      this.notifyPropertyChange('currentTimePulse');
+      this.currentTimeMetronome();
+    }, interval);
+  }.on('init'),
+
+  currentTime: function() {
+    var date = new Date(2015, 5, 27, 10, 0);
+    var units = countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS;
+    return countdown(date, null, units).toString().replace(/(,|and )/g, '').replace(/([A-Za-z]+)/g, '<span class="words">$1</span>');
+  }.property('currentTimePulse')
 
 });
