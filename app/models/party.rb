4f9860a8621efe87c3b883e6d25a8fa1b559b52b
Robin Joseph <robinjoseph08@sbcglobal.net>
2015-03-21 11:59:37 -0500

    add generate_code to party

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,11 +1,12 @@
 class Party < ActiveRecord::Base
 
-  after_save :send_notification_email
+  before_create :generate_code
+  after_save    :send_notification_email
 
   has_many :guests
 
   validates :email,
-            :format   => {
+            :format => {
               :with    => /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\z/,
               :message => "isn't valid"
             },
@@ -15,6 +16,22 @@ class Party < ActiveRecord::Base
 
   def send_notification_email
     Mailer.notify_party_update(self).deliver_now
+  end
+
+  private
+
+  def generate_code
+    if self.code.blank?
+      a    = ('A'..'Z').to_a+(0..9).to_a
+      code = nil
+      a.delete 'I'
+
+      while code.nil? || Party.exists?(:code => code)
+        code = (1..5).collect { a[rand(a.size)] }.join
+      end
+
+      self.code = code
+    end
   end
 
 end
