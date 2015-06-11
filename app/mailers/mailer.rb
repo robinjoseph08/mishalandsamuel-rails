6caa30d23c583fb417edd4e58cd3f672f0a9124f
Robin Joseph <robinjoseph08@sbcglobal.net>
2015-06-10 20:12:33 -0700

    update csv email

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -16,6 +16,28 @@ class Mailer < ActionMailer::Base
   end
 
   def send_csv path
+    @data = {}
+    @data[:unconfirmed] = {
+      :total => Guest.where(:response => 0).count,
+      :mathew => Guest.joins(:party).where(:response => 0, :parties => { :label => 0 }).count,
+      :johny => Guest.joins(:party).where(:response => 0, :parties => { :label => 1 }).count
+    }
+    @data[:rsvp] = {
+      :total => Guest.all.count - @data[:unconfirmed][:total],
+      :mathew => Guest.joins(:party).where(:parties => { :label => 0 }).count - @data[:unconfirmed][:mathew],
+      :johny => Guest.joins(:party).where(:parties => { :label => 1 }).count - @data[:unconfirmed][:johny]
+    }
+    @data[:not_attending] = {
+      :total => Guest.where(:response => 2).count,
+      :mathew => Guest.joins(:party).where(:response => 2, :parties => { :label => 0 }).count,
+      :johny => Guest.joins(:party).where(:response => 2, :parties => { :label => 1 }).count
+    }
+    @data[:attending] = {
+      :total => Guest.where(:response => 1).count,
+      :mathew => Guest.joins(:party).where(:response => 1, :parties => { :label => 0 }).count,
+      :johny => Guest.joins(:party).where(:response => 1, :parties => { :label => 1 }).count
+    }
+
     attachments['master.csv'] = File.read path
     mail :to      => [ENV['ROBIN_EMAIL_ADDRESS'], ENV['MAS_EMAIL_ADDRESS'], ENV['DAD_EMAIL_ADDRESS']],
          :from    => "Wedding Updates <#{ENV['MAS_EMAIL_ADDRESS']}>",
